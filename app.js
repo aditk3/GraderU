a0aeb90b596e24aa21bb3d0c61281edbbca75eac
Adit Kapoor <aditkapoor00@gmail.com>
2023-01-11 03:19:08 +0530

    Reformatted code

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-const express = require("express");
-const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const compression = require("compression");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const hpp = require("hpp");
-const cors = require("cors");
-const path = require("path");
-const AppError = require("./backend/utils/appError");
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const compression = require('compression');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
+const path = require('path');
+const AppError = require('./backend/utils/appError');
 const {
-  extractJWTCookieToHeader,
-} = require("./backend/utils/customMiddleware");
-const globalErrorHandler = require("./backend/controllers/errorController");
+    extractJWTCookieToHeader,
+} = require('./backend/utils/customMiddleware');
+const globalErrorHandler = require('./backend/controllers/errorController');
 
-const userRouter = require("./backend/routes/userRoutes");
-const courseRouter = require("./backend/routes/courseRoutes");
-const professorRouter = require("./backend/routes/professorRoutes");
-const reviewRouter = require("./backend/routes/reviewRoutes");
+const userRouter = require('./backend/routes/userRoutes');
+const courseRouter = require('./backend/routes/courseRoutes');
+const professorRouter = require('./backend/routes/professorRoutes');
+const reviewRouter = require('./backend/routes/reviewRoutes');
 
 const app = express();
 
 // middleware
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    })
 );
-app.options("*", cors());
+app.options('*', cors());
 app.use(helmet()); // Set security HTTP headers
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev")); // middleware for logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev')); // middleware for logging
 }
 
 const limiter = rateLimit({
-  max: 500,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour!",
+    max: 500,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!',
 });
 
-app.use("/api", limiter); // Rate limiter
-app.use(express.json({ limit: "10kb" })); // Body parser
+app.use('/api', limiter); // Rate limiter
+app.use(express.json({ limit: '10kb' })); // Body parser
 app.use(mongoSanitize()); // Data sanitization against NoSQL query injection
 app.use(xss()); // Data sanitization against XSS
 
@@ -51,22 +51,24 @@ app.use(extractJWTCookieToHeader()); // extract jwt cookie and set authorization
 app.use(compression());
 
 // routes
-app.use("/api/v1/users", userRouter); // connects user/auth routes
-app.use("/api/v1/courses", courseRouter); // connects course routes
-app.use("/api/v1/professors", professorRouter); // connects professor routes
-app.use("/api/v1/reviews", reviewRouter); // connects professor routes
+app.use('/api/v1/users', userRouter); // connects user/auth routes
+app.use('/api/v1/courses', courseRouter); // connects course routes
+app.use('/api/v1/professors', professorRouter); // connects professor routes
+app.use('/api/v1/reviews', reviewRouter); // connects professor routes
 
 // Serving static files
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, 'frontend', 'build', 'index.html')
+        );
+    });
 }
 
 // handles unknown routes
-app.all("*", (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 // connects error handler
--- server.js
+++ server.js
@@ -1,44 +1,44 @@
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 // handle 500 server errors. Shutsdown server.
-process.on("uncaughtException", (err) => {
-  console.log("Uncaught Exception! Shutting down...");
-  console.log(err.name, err.message);
-  process.exit(1);
+process.on('uncaughtException', (err) => {
+    console.log('Uncaught Exception! Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
 });
 
 // consume enviornment variables
-dotenv.config({ path: "./config.env" });
-const app = require("./app");
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 // mongo connection string
 const DB = process.env.DATABASE.replace(
-  "<password>",
-  process.env.DATABASE_PASSWORD
+    '<password>',
+    process.env.DATABASE_PASSWORD
 );
 
 // connect to DB
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB connection successful!");
-  });
+    .connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('DB connection successful!');
+    });
 
 // defines port for server to listen on
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+    console.log(`App running on port ${port}...`);
 });
 
 // Gracefully handles unknown errors by shutting down server
-process.on("uncaughtRejection", (err) => {
-  console.log("Unhandled Rejection! Shutting down...");
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
+process.on('uncaughtRejection', (err) => {
+    console.log('Unhandled Rejection! Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 });
